9fa813bda9fb51fa9fb5a8bb0950070715fe1b9a
Alicia S <alicia.rsouza@gmail.com>
2023-06-22 20:33:25 -0300

    :test_tube: test: Criando testes de inputs textuais

--- src/main/java/CampoDeTreinamento.java
+++ src/main/java/CampoDeTreinamento.java
@@ -1,12 +1,34 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CampoDeTreinamento {
     @Test
-    public  void testandoCamposDeTexto() {
+    public  void testeCampoDeInputTexto() {
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver chrome = new ChromeDriver();
         chrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        chrome.findElement(By.id("elementosForm:nome")).sendKeys("Alicia");
+        chrome.findElement(By.id("elementosForm:sobrenome")).sendKeys("Souza");
+        String resultadoNome = chrome.findElement(By.id("elementosForm:nome")).getAttribute("value");
+        String resultadoSobrenome = chrome.findElement(By.id("elementosForm:sobrenome")).getAttribute("value");
+
+        Assert.assertEquals("Alicia", resultadoNome);
+        Assert.assertEquals("Souza", resultadoSobrenome);
+        chrome.quit();
+    }
+
+    @Test
+    public void testeCampoDeTextoArea (){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver chrome = new ChromeDriver();
+        chrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        chrome.findElement(By.id("elementosForm:sugestoes")).sendKeys("Testando o campo de texto");
+        String resultadoSugestao = chrome.findElement(By.id("elementosForm:sugestoes")).getAttribute("value");
+
+        Assert.assertEquals(resultadoSugestao, "Testando o campo de texto");
+        chrome.quit();
     }
 }
