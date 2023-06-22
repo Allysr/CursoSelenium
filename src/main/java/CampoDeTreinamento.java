ae892e8ecc73a94be55c117cdae33e97d4c857fb
Alicia S <alicia.rsouza@gmail.com>
2023-06-22 19:44:56 -0300

    :recycle: refactor: Alterando estrutura para iniciar o estudo

--- src/main/java/CampoDeTreinamento.java
+++ src/main/java/CampoDeTreinamento.java
@@ -1,13 +1,12 @@
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Main {
-    public static void main(String[] args) {
+public class CampoDeTreinamento {
+    @Test
+    public  void testandoCamposDeTexto() {
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver chrome = new ChromeDriver();
-        chrome.get("http:///google.com");
-        System.out.printf(chrome.getTitle());
-
-
+        chrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
     }
 }
