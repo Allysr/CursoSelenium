125d8b3ede3a4d7acf7f18c5773cbc0bd7f672d4
Alicia S <alicia.rsouza@gmail.com>
2023-06-25 14:36:16 -0300

    :test_tube: test: Criando testes e fazendo o desafio

---
+++ src/main/java/CampoDeTreinamentoAlerts.java
@@ -0,0 +1,67 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CampoDeTreinamentoAlerts {
+
+    @Test
+    public void validarAlertSimples(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver chrome = new ChromeDriver();
+        chrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        chrome.findElement(By.id("alert")).click();
+        Alert alerta = chrome.switchTo().alert();
+
+        Assert.assertEquals("Alert Simples", alerta.getText());
+        alerta.accept();
+        chrome.quit();
+    }
+
+    @Test
+    public void validarAlertConfirmar(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver chrome = new ChromeDriver();
+        chrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        chrome.findElement(By.id("confirm")).click();
+        Alert alerta = chrome.switchTo().alert();
+
+        Assert.assertEquals("Confirm Simples", alerta.getText());
+        alerta.accept();
+        Assert.assertEquals("Confirmado", alerta.getText());
+        alerta.accept();
+    }
+
+    @Test
+    public void validarAlertConfirmarNegativo(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver chrome = new ChromeDriver();
+        chrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        chrome.findElement(By.id("confirm")).click();
+        Alert alerta = chrome.switchTo().alert();
+
+        Assert.assertEquals("Confirm Simples", alerta.getText());
+        alerta.dismiss();
+        Assert.assertEquals("Negado", alerta.getText());
+        alerta.accept();
+        chrome.quit();
+    }
+
+    @Test
+    public void validarAlertPrompt(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver chrome = new ChromeDriver();
+        chrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        chrome.findElement(By.id("prompt")).click();
+        Alert alerta = chrome.switchTo().alert();
+        alerta.sendKeys("Testando alerta");
+        alerta.accept();
+        Assert.assertEquals("Era Testando alerta?", alerta.getText());
+        alerta.accept();
+        Assert.assertEquals(":D", alerta.getText());
+        alerta.accept();
+        chrome.quit();
+    }
+}
---
+++ src/main/java/DesafioCadastroCompleto.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DesafioCadastroCompleto {
+   @Test
+    public void cadastroCompleto(){
+       System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+       WebDriver chrome = new ChromeDriver();
+       chrome.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+       chrome.findElement(By.id("elementosForm:nome")).sendKeys("Alicia");
+       chrome.findElement(By.id("elementosForm:sobrenome")).sendKeys("Souza");
+       chrome.findElement(By.id("elementosForm:sexo:1")).click();
+       chrome.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+
+       WebElement campoEscolaridade = chrome.findElement(By.id("elementosForm:escolaridade"));
+       Select escolaridade = new Select(campoEscolaridade);
+       escolaridade.selectByValue("superior");
+
+       WebElement campoEsportes = chrome.findElement(By.id("elementosForm:esportes"));
+       Select esportes = new Select(campoEsportes);
+       esportes.selectByValue("Corrida");
+
+       chrome.findElement(By.id("elementosForm:sugestoes")).sendKeys("Gostaria de deixar sugerido um novo campo.");
+
+       chrome.findElement(By.id("elementosForm:cadastrar")).click();
+
+
+       Assert.assertEquals("Cadastrado!", chrome.findElement(By.cssSelector("div > span")).getText());
+       Assert.assertEquals("Nome: Alicia", chrome.findElement(By.id("descNome")).getText());
+       Assert.assertEquals("Sobrenome: Souza", chrome.findElement(By.id("descSobrenome")).getText());
+       Assert.assertEquals("Sexo: Feminino", chrome.findElement(By.id("descSexo")).getText());
+       Assert.assertEquals("Comida: Pizza", chrome.findElement(By.id("descComida")).getText());
+       Assert.assertEquals("Escolaridade: superior", chrome.findElement(By.id("descEscolaridade")).getText());
+       Assert.assertEquals("Esportes: Corrida", chrome.findElement(By.id("descEsportes")).getText());
+       Assert.assertEquals("Sugestoes: Gostaria de deixar sugerido um novo campo.", chrome.findElement(By.id("descSugestoes")).getText());
+
+       chrome.quit();
+
+   }
+
+}
